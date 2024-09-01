29850c29d31462a7c572316967956825aa7ab765
harrystephen106 <harrystephen315@gmail.com>
2024-09-01 22:19:37 +0530

    new file

---
+++ python try/day 2/ifelse 5.py
@@ -0,0 +1,10 @@
+a=int(input("enter your first marke"))
+b=int(input("enter your second mark"))
+c=int(input("enter your third mark"))
+d=int(input("enter your fourth mark"))
+e=int(input("enter your fifth mark"))
+f=(a+b+c+d+e)/5
+if(f<35):
+    print("additional class required")
+else:
+    print("you are good to go")
