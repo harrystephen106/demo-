2cbb6b9c9b2c53a3763942d7cfaf90b459c10a47
harrystephen106 <harrystephen315@gmail.com>
2024-09-01 14:03:17 +0530

    new file

---
+++ python try/day 1/casting 2.py
@@ -0,0 +1,25 @@
+# program 1
+
+name=input()
+age=input()
+print("my name is:",name)
+print("my age is:",age)
+
+#program 2
+
+name=input()
+age=input()
+address=input()
+print("my name is:",name)
+print("my age is:",age)
+print("my address is:",address)
+
+# program 3
+
+a=int(input())
+b=int(input())
+c=int(input())
+d=a*b*c
+e=a+b+c
+f=d/e
+print("divited value",f)
---
+++ python try/day 1/casting 3.py
@@ -0,0 +1,8 @@
+# program 4
+
+name=input()
+score=int(input())
+dep=input()
+print("my name is:",name)
+print("my score is:",score/10,"/10")
+print("my department is:",dep)
