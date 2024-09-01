2581e3bc0038c3ac83a9d74c6f885a9ecbcbe534
harrystephen106 <harrystephen315@gmail.com>
2024-09-01 15:14:50 +0530

    new file

--- python try/day 2/ifele 1.py
+++ python try/day 2/ifele 1.py
@@ -5,3 +5,12 @@ if(mark>35):
     print("you pass")
 else:
     print("you are fail")
+
+
+# program 2
+
+income=int(input("please enter your income here =>"))
+if(income>7000):
+    print("no eligible for scolership")
+else:
+    print("eligible for scolership ")
