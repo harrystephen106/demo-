25217b9d98efd9e3bfdcc420daab7c376840b170
harrystephen106 <harrystephen315@gmail.com>
2024-09-02 21:28:24 +0530

    day 3 file added

---
+++ python try/day 3/forloop 1.py
@@ -0,0 +1,17 @@
+# program 2
+
+# get input value of 8,15
+
+a=int(input("A value :"))      
+b=int(input("B value :"))    
+for i in range(a+1,b):      # the output show 9,10,11,12,13,14 if output show 8 extra  
+    print(i)                # then add a+1 becouse the a+1 is add one value in "a"0
+
+# program 3
+
+for i in range(1,11):
+    if(i%2==0):
+        print(i)
+
+# program 4
+
---
+++ python try/day 3/forloop.py
@@ -0,0 +1,17 @@
+# ex 1 for loop 
+
+for i in "apple":
+    print(i)          # i is variable
+
+# ex 2
+
+for i in range(5):     # range 5 is calculate index value of 0--4 (0,1,2,3,4)
+    print(i)         
+
+# program 1
+
+for i in range(1,11):
+    print(i,"x2=",i*2)
+
+for i in range(1,11):
+    print(i,"x3=",i*3)
